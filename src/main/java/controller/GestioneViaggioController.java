86b8d575df77e79a5cfd99c86d19c83833f50421
Lucaa0202 <lucabelforte3@gmail.com>
2026-06-23 20:12:42 +0200

    modifiche

--- src/main/java/controller/GestioneViaggioController.java
+++ src/main/java/controller/GestioneViaggioController.java
@@ -1,14 +1,23 @@
 package controller;
 
-
+import beans.VeicoloBean;
 import beans.ViaggioBean;
+import dao.VeicoloDAO;
+import dao.ViaggioDAO;
+import dao.full.sql.VeicoloDAOSQL;
+import dao.full.sql.ViaggioDAOSQL;
+import exceptions.NoResultException;
+import model.Veicolo;
 import model.Viaggio;
+import utilities.other.mappers.VeicoloMapper;
 import utilities.other.mappers.ViaggioMapper;
+
+import java.util.List;
 
 public class GestioneViaggioController {
 
     // Quando creeremo il ViaggioDAO, toglieremo il commento a questa riga
-    // private final ViaggioDAO viaggioDAO = new ViaggioDAOSQL();
+    private final ViaggioDAO viaggioDAO = new ViaggioDAOSQL();
 
     public void creaViaggio(ViaggioBean viaggioBean) throws Exception {
 
@@ -17,10 +26,27 @@ public class GestioneViaggioController {
         Viaggio viaggioModel = mapper.fromBeanToModel(viaggioBean);
 
         // 2. Chiamata al database (DA ATTIVARE NEL PROSSIMO STEP)
-        // viaggioDAO.inserisciViaggio(viaggioModel);
+        viaggioDAO.inserisciViaggio(viaggioModel);
 
         // Log di conferma per IntelliJ
         System.out.println("GestioneViaggioController: Viaggio da " + viaggioModel.getPartenza() +
                 " verso " + viaggioModel.getDestinazione() + " arrivato al controller!");
+    }
+
+    // --- NUOVO METODO PER IL CARICAMENTO AUTO ---
+    public void recuperaVeicoliUtente(String email, List<VeicoloBean> veicoliBeanList) throws Exception {
+
+        // 1. Istanziamo il DAO (In futuro qui potremmo usare una FactoryDAO)
+        VeicoloDAO veicoloDAO = new VeicoloDAOSQL();
+
+        // 2. Chiamiamo il DB per avere la lista dei Model
+        List<Veicolo> veicoliModel = veicoloDAO.trovaVeicoliPerProprietario(email);
+
+        // 3. Trasformiamo i Model in Bean usando il Mapper
+        VeicoloMapper mapper = new VeicoloMapper();
+
+        for (Veicolo veicolo : veicoliModel) {
+            veicoliBeanList.add(mapper.fromModelToBean(veicolo));
+        }
     }
 }
--- src/main/java/view/gui/OffriPassaggioGUI.java
+++ src/main/java/view/gui/OffriPassaggioGUI.java
@@ -1,8 +1,9 @@
 package view.gui;
 
-
+import beans.VeicoloBean;
 import beans.ViaggioBean;
 import controller.GestioneViaggioController;
+import exceptions.NoResultException;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
@@ -15,6 +16,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OffriPassaggioGUI extends CommonGUI {
 
@@ -24,49 +27,91 @@ public class OffriPassaggioGUI extends CommonGUI {
 
     private final GestioneViaggioController gestioneViaggioController = new GestioneViaggioController();
 
-    @FXML private TextField partenza;
-    @FXML private TextField arrivo;
+    // Adesso partenza e arrivo sono ComboBox!
+    @FXML private ComboBox<String> partenza;
+    @FXML private ComboBox<String> arrivo;
     @FXML private DatePicker dataPartenza;
     @FXML private TextField oraPartenza;
     @FXML private TextField posti;
     @FXML private TextField prezzo;
-    @FXML private ComboBox<String> tendinaVeicoli; // Qui caricheremo le targhe
+    @FXML private ComboBox<String> tendinaVeicoli;
     @FXML private Text messaggio;
 
     @FXML
     public void initialize() {
-        // TODO: Appena apriamo la pagina, dovremmo caricare le targhe dal DB nella tendina.
-        // Per ora mettiamo dei dati finti (stub) per vedere se la grafica funziona!
-        tendinaVeicoli.getItems().addAll("AB123CD - Fiat Panda", "EF456GH - Ford Fiesta");
+        tendinaVeicoli.getItems().clear();
+        partenza.getItems().clear();
+        arrivo.getItems().clear();
+
+        // 1. Popoliamo gli snodi principali per il Carpooling
+        String[] macroZone = {
+                "Città Universitaria",
+                "Stazione Termini",
+                "Stazione Tiburtina",
+                "Piazza Bologna",
+                "San Giovanni",
+                "EUR Magliana",
+                "Roma Nord",
+                "Roma Sud"
+        };
+        partenza.getItems().addAll(macroZone);
+        arrivo.getItems().addAll(macroZone);
+
+        // 2. Caricamento dinamico dei veicoli dell'utente dal DB
+        try {
+            String emailUtente = session.getUser().getCredenziali().getEmail();
+            List<VeicoloBean> veicoliUtente = new ArrayList<>();
+            gestioneViaggioController.recuperaVeicoliUtente(emailUtente, veicoliUtente);
+
+            for (VeicoloBean v : veicoliUtente) {
+                String voce = v.getTarga() + " - " + v.getMarca() + " " + v.getModello();
+                tendinaVeicoli.getItems().add(voce);
+            }
+
+            if (!tendinaVeicoli.getItems().isEmpty()) {
+                tendinaVeicoli.getSelectionModel().selectFirst();
+            }
+
+        } catch (NoResultException e) {
+            tendinaVeicoli.setPromptText("Nessuna auto registrata");
+        } catch (Exception e) {
+            mostraErrore("Errore durante il caricamento dei veicoli.");
+        }
     }
 
     @FXML
     private void salvaViaggio(MouseEvent event) {
         messaggio.setVisible(false);
         try {
-            // 1. Uniamo Data e Ora in un LocalDateTime
+            // Controllo rigoroso sulle nuove tendine
+            String partenzaScelta = partenza.getValue();
+            String arrivoScelto = arrivo.getValue();
+
+            if (partenzaScelta == null || arrivoScelto == null) {
+                throw new Exception("Seleziona una zona di partenza e di arrivo.");
+            }
+            if (partenzaScelta.equals(arrivoScelto)) {
+                throw new Exception("La partenza e l'arrivo non possono coincidere.");
+            }
+
             LocalDate data = dataPartenza.getValue();
             if (data == null) throw new Exception("Seleziona una data.");
 
-            // Format per l'orario (HH:mm)
             LocalTime ora = LocalTime.parse(oraPartenza.getText(), DateTimeFormatter.ofPattern("HH:mm"));
             LocalDateTime dataOraCompleta = LocalDateTime.of(data, ora);
 
-            // 2. Parsiamo numeri
             int postiDisponibili = Integer.parseInt(posti.getText());
             double prezzoScelto = Double.parseDouble(prezzo.getText());
 
-            // 3. Recuperiamo la targa dalla tendina (es: taglia la stringa "AB123CD - Fiat Panda")
             String veicoloSelezionato = tendinaVeicoli.getValue();
             if (veicoloSelezionato == null) throw new Exception("Seleziona un veicolo.");
-            String targa = veicoloSelezionato.split(" ")[0]; // Prende solo la prima parola (la targa)
+            String targa = veicoloSelezionato.split(" ")[0];
 
-            // 4. Creiamo il Bean (assicurati di aver aggiunto la targa al tuo ViaggioBean!)
-            // L'idViaggio mettiamo 0 perché lo genererà automaticamente MySQL (AUTO_INCREMENT)
             String email = session.getUser().getCredenziali().getEmail();
-            ViaggioBean viaggioBean = new ViaggioBean(0, email, partenza.getText(), arrivo.getText(), dataOraCompleta, postiDisponibili, prezzoScelto, targa);
 
-            // 5. Passiamo al controller applicativo (che creeremo al prossimo passo)
+            // Creazione del Bean con i dati sicuri
+            ViaggioBean viaggioBean = new ViaggioBean(0, email, partenzaScelta, arrivoScelto, dataOraCompleta, postiDisponibili, prezzoScelto, targa);
+
             gestioneViaggioController.creaViaggio(viaggioBean);
 
             messaggio.setFill(javafx.scene.paint.Color.GREEN);
@@ -78,6 +123,7 @@ public class OffriPassaggioGUI extends CommonGUI {
         } catch (NumberFormatException e) {
             mostraErrore("Posti e prezzo devono essere numeri validi.");
         } catch (Exception e) {
+            e.printStackTrace();
             mostraErrore(e.getMessage());
         }
     }
